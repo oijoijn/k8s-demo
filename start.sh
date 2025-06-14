d1bfe99a5a16ff6ca5e387e5c9a3fe151936780b
so <jjjjjjjjjjjjj@jjjjj.com>
2025-06-15 01:21:43 +0900

    change

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ kubectl apply -f backend/
 kubectl apply -f frontend/
 kubectl apply -f networkpolicies.yaml
 # Linux (WSL)
-kubectl port-forward -n app-demo svc/web 8080:80 --address 0.0.0.0
+# kubectl port-forward -n app-demo svc/web 8080:80 --address 0.0.0.0

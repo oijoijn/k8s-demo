ae2e9deb6d4fb391d7d466e2bd0a7075a630cf93
so <jjjjjjjjjjjjj@jjjjj.com>
2025-06-14 21:18:24 +0900

    portforwardの追加

--- start.sh
+++ start.sh
@@ -4,3 +4,5 @@ kubectl apply -f mysql/
 kubectl apply -f backend/
 kubectl apply -f frontend/
 kubectl apply -f networkpolicies.yaml
+# Linux (WSL)
+kubectl port-forward -n app-demo svc/web 8080:80 --address 0.0.0.0
